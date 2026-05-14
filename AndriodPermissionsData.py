d4bd62b6da3f1f46bafab158538ecf92b0f2608d
Amorian14 <112989021+Amorian14@users.noreply.github.com>
2026-05-14 15:07:09 -0400

    Add imports for threading, wx, pandas, numpy, and matplotlib
    Added necessary imports for threading, wx, pandas, numpy, and matplotlib.

--- AndriodPermissionsData.py
+++ AndriodPermissionsData.py
@@ -1,1 +1,13 @@
+import threading
+import os
 
+import wx
+import wx.grid as gridlib
+
+import pandas as pd
+import numpy as np
+
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+SAMPLE_ROWS = 100
